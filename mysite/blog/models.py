89128a969f5a6ac5bac01847caa7fe594fabb4a0
Robert Smith <rsmith2271@live.co.uk>
2025-09-20 16:48:13 +0100

    20/09/2025 16:48

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,12 @@ from django.utils import timezone
 from django.conf import settings
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return (super().get_queryset().filter(status = Post.Status.PUBLISHED))
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
@@ -17,6 +23,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']
