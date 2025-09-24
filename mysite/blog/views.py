8a850c11fa4c56c5dac6c46b9280c1cadec6f5e5
Robert Smith <rsmith2271@live.co.uk>
2025-09-24 21:00:19 +0100

    24/09/2025 21:00

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
-from django.http import Http404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
